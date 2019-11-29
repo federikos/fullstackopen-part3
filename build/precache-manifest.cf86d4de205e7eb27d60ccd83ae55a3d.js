999ccbe3942e48d4d4f8d0d330b5e68a1b18180d
federikos <federikos@bk.ru>
2019-11-29 21:08:57 +0300

    complete exercise #3.11 phonebook full stack

---
+++ build/precache-manifest.cf86d4de205e7eb27d60ccd83ae55a3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a6ca8faaafa165847c778a017e584cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93fb710ae0215eeeb469",
+    "url": "/static/css/main.41f74137.chunk.css"
+  },
+  {
+    "revision": "e72bc4bdbda6a25f7a29",
+    "url": "/static/js/2.cec21e00.chunk.js"
+  },
+  {
+    "revision": "93fb710ae0215eeeb469",
+    "url": "/static/js/main.d3ba52d1.chunk.js"
+  },
+  {
+    "revision": "55cfac3c0dbb3c6076fc",
+    "url": "/static/js/runtime~main.94b6b7b3.js"
+  }
+]);
