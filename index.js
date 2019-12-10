1bb6caf61de65604581a288278b0078dedb7f592
federikos <federikos@bk.ru>
2019-12-10 17:25:43 +0300

    complete exercise #3.16: Phonebook database, step3

--- index.js
+++ index.js
@@ -17,40 +17,44 @@ app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons);
-  })
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      res.json(persons);
+    })
+    .catch(err => next(err))
 })
 
-app.get('/api/info', (req, res) => {
-  Person.find({}).then(persons => {
-    res.send(`
-      <p>Phonebook has info for ${persons.length} people</p>
-      ${new Date()}
-    `);
-  })
+app.get('/api/info', (req, res, next) => {
+  Person.find({})
+    .then(persons => {
+      res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
+        ${new Date()}
+      `);
+    })
+  .catch(err => next(err))
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const contact = persons.find(person => person.id === id);
-  if (contact) {
-    res.json(contact)
-  } else {
-    res.status(404).end()
-  }
-})
+// app.get('/api/persons/:id', (req, res) => {
+//   const id = Number(req.params.id);
+//   const contact = persons.find(person => person.id === id);
+//   if (contact) {
+//     res.json(contact)
+//   } else {
+//     res.status(404).end()
+//   }
+// })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
     })
-    .catch(err => console.log(err))
+    .catch(err => next(err))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const person = req.body;
 
   if (!person.name) {
@@ -66,11 +70,25 @@ app.post('/api/persons', (req, res) => {
     number: person.number,
   });
 
-  newPerson.save().then(person => {
-    console.log(`added ${person.name} number ${person.number} to phonebook`);
-    res.json(person);
-  })
+  newPerson.save()
+    .then(person => {
+      console.log(`added ${person.name} number ${person.number} to phonebook`);
+      res.json(person);
+    })
+    .catch(err => next(err))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
